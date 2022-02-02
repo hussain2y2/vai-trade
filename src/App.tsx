75cab98522d5505a7e56d8c383337edc249f3c4a
Hussain Ahmad <engrhusainahmad@gmail.com>
2022-02-02 18:32:19 +0500

    Removing extra files

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {RootState} from './store';
 
 function App() {
   const {detail} = useSelector((state: RootState) => state.starship);
+
   return (
     <div className="p-3 bg-gray-50 h-screen min-h-full">
       {Object.keys(detail).length ? <Detail/> : <Listing/>}
