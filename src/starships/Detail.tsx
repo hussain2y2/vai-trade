6bff7fd2b234a4c1877345e10d00d17197a5f83a
Hussain Ahmad <engrhusainahmad@gmail.com>
2022-02-02 15:17:15 +0500

    Showing fleet in detail page

--- src/starships/Detail.tsx
+++ src/starships/Detail.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
-import {useSelector} from "react-redux";
-import {RootState} from "../store";
+import {useSelector} from 'react-redux';
+import {RootState} from '../store';
+import ProgressBar from './ProgressBar';
+import {selectDetail} from './starshipSlice';
+import {MinusIcon, PlusIcon} from '@heroicons/react/solid';
 
 const Detail = ()  => {
-  const {detail} = useSelector((state: RootState) => state.starship);
-  console.log(detail)
+  const {detail}: any = useSelector((state: RootState) => state.starship);
   return(
-    <div>
-      Detail
+    <div className="flex justify-start bg-white max-w-full py-4 px-3 mx-auto mx-5 border border-b-gray-200 rounded-lg">
+      <div className="py-4 px-3">
+        <h1 className="text-4xl font-bold">Detail View</h1>
+        <div className="flex flex-col mt-5">
+          <div className="text-sm font-medium text-white bg-gray-400 w-fit rounded-lg px-2 py-0.5 mb-2">
+            {detail.name}
+          </div>
+          <div className="text-sm font-medium text-white bg-gray-300 w-64 rounded-lg px-2 py-0.5 mb-2">
+            {detail.manufacturer}
+          </div>
+          <div className="flex justify-center items-center mt-10 bg-gray-100 rounded-lg p-4" >
+            <ProgressBar capacity={1000} crew={120} passengers={500}/>
+            <button className="mt-1 font-bold text-white w-8 bg-teal-400 rounded-lg p-1 ml-6 mr-4">
+              <PlusIcon className="w-6 h-6"/>
+            </button>
+            <button className="mt-1 font-bold text-white w-8 bg-red-300 rounded-lg p-1">
+              <MinusIcon className="w-6 h-6"/>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/starships/Listing.tsx
+++ src/starships/Listing.tsx
@@ -1,12 +1,12 @@
 import React, {useState} from 'react';
 // Hero Icons
-import {PencilIcon, PlusIcon} from '@heroicons/react/solid';
+import {PencilIcon, PlusIcon, SearchIcon} from '@heroicons/react/solid';
 import {useDispatch, useSelector} from 'react-redux';
-// starship reducer actions
+// starship reducer actions and Root State
 import {addFleet, selectDetail} from './starshipSlice';
+import {RootState} from '../store';
 // Custom utils
 import {Api, useDebouncedEffect} from '../utils';
-import {RootState} from '../store';
 import ProgressBar from './ProgressBar';
 
 const Listing = () => {
@@ -15,7 +15,7 @@ const Listing = () => {
 
   let url = '/api/starships';
 
-  // Debounced effect
+  // Debounced effect for fetching starships from swapi.
   useDebouncedEffect(() => Api.get(searchText ? `${url}/?search=${searchText}` : url)
       .then(response => {
         const {results}: any = response.data;
@@ -41,24 +41,21 @@ const Listing = () => {
         <div className="relative text-gray-600 focus-within:text-gray-400 mt-5">
           <span className="absolute inset-y-0 left-0 flex items-center pl-2">
             <button type="submit" className="p-1 focus:outline-none focus:shadow-outline">
-              <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                   viewBox="0 0 24 24" className="w-6 h-6">
-                <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
-              </svg>
+              <SearchIcon className="w-6 h-6"/>
             </button>
           </span>
           <input
             type="search"
-            className="py-3 w-8/12 text-sm text-gray-600 bg-white border-1 rounded-md pl-10 focus:outline-none focus:bg-white focus:text-gray-600"
+            className="py-3 w-8/12 text-sm text-gray-600 bg-gray-200 border-1 rounded-md pl-10 focus:outline-none focus:text-gray-600"
             placeholder="Search..." autoComplete="off"
             onChange={(e) => setSearchText(e.target.value)}
           />
         </div>
         {/* List Searched Starships */}
         {starships && <div className="border-gray-200 mt-5 w-8/12">
-          <div className="bg-gray-100 rounded-xl mb-4">
+          <div className="bg-gray-200 rounded-xl mb-4">
             {starships.map((starship: any, key) => <div
-              className="flex justify-between pl-3 pr-4 py-4 rounded-xl hover:bg-white" key={key}>
+              className="flex justify-between pl-3 pr-4 py-4 rounded-xl hover:bg-gray-100" key={key}>
               <div className="text-sm font-medium text-white w-9/12 bg-gray-400 rounded-lg p-2">{starship.name}</div>
               <button
                 className="mt-1 font-bold text-white w-9 bg-teal-400 rounded-lg p-2"
@@ -71,7 +68,7 @@ const Listing = () => {
         </div>}
       </div>
       <div className="w-3/6 mr-5">
-        <div className="py-4 px-3 mx-auto bg-white rounded-xl">
+        <div className="py-4 px-3 mx-auto bg-white rounded-xl border border-gray-200">
           <h3 className="text-2xl font-bold mx-5">Your Fleet</h3>
           {fleets.map((fleet: any, key: number) => <div className="flex justify-start items-center mx-5 mt-5" key={key}>
             <button className="font-bold w-14 h-14 bg-gray-400 rounded-full p-4 mr-3" onClick={() => dispatch(selectDetail(fleet))}>
